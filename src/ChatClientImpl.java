70ffd4fd9c2af46ba1b9a51dce1f2f51002d8a8a
wandertorres <wander_torres@live.com>
2016-11-16 08:04:29 -0300

    Finalizado...

--- src/ChatClientImpl.java
+++ src/ChatClientImpl.java
@@ -1,29 +1,57 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 public class ChatClientImpl extends UnicastRemoteObject implements ChatClient {
+	private static final long serialVersionUID = 1L;
+	private static ChatServer servidor;
+	private static String nickname;
+	private static Scanner teclado = new Scanner(System.in);
+	
 	protected ChatClientImpl() throws RemoteException {
 		super();
 	}
 
-	private static final long serialVersionUID = 1L;
-	private ChatServer servidor;
-	private String nickname;
+	public static void entrar(ChatClient cliente) throws MalformedURLException, RemoteException, NotBoundException {
+		servidor = (ChatServer) Naming.lookup("//localhost/chat");
+		while(!(nickDisponivel(servidor)))
+			System.out.println("Nick não disponível. Tente outro.");
+		servidor.conectar(nickname, cliente);
+		limparTela();
+		cliente.evento("Você entrou na sala como " +nickname+ "\n");
+		while(falar(servidor));
+	}
 	
-	public ChatServer getServidor() {
-		return servidor;
+	private static boolean nickDisponivel(ChatServer servidor) throws RemoteException {
+		System.out.print("Nickname: ");
+		nickname = teclado.nextLine();
+		String[] nicks = servidor.naSessao();
+		for(int i=0; i<nicks.length; i++)
+			if(nicks[i].equals(nickname))
+				return false;
+		return true;
 	}
-
-	public void setServidor(ChatServer servidor) {
-		this.servidor = servidor;
+	
+	private static void limparTela() {
+		for(int i=0; i<50; i++)
+			System.out.println("\n");
 	}
-
-	public String getNickname() {
-		return nickname;
+	
+	private static boolean falar(ChatServer servidor) throws RemoteException {
+		String msg = teclado.nextLine();
+		if(msg.equals("Sair"))
+			return false;
+		servidor.falar(nickname, msg);
+		return true;
 	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
+	
+	public static void sair(ChatClient cliente) throws RemoteException, MalformedURLException, NotBoundException {
+		servidor.desconectar(nickname, cliente);
+		cliente.evento("Você saiu da sala.");
+		System.exit(0);
 	}
 	
 	@Override
--- src/Cliente.java
+++ src/Cliente.java
@@ -1,20 +1,14 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.util.Scanner;
 
 public class Cliente {
-	private static Scanner teclado = new Scanner(System.in);
-	private static String nickname;
-	
 	public static void main(String[] args) throws IOException {
 		try {
 			ChatClient cliente = (ChatClient) new ChatClientImpl();
-			ChatServer servidor = (ChatServer) Naming.lookup("//localhost/chat");
-			entrar(cliente, servidor);
-			//sair();
+			ChatClientImpl.entrar(cliente);
+			ChatClientImpl.sair(cliente);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
@@ -22,33 +16,5 @@ public class Cliente {
 		} catch (NotBoundException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	private static boolean nickDisponivel(ChatServer servidor) throws RemoteException {
-		System.out.print("Nickname: ");
-		nickname = teclado.nextLine();
-		String[] nicks = servidor.naSessao();
-		for(int i=0; i<nicks.length; i++)
-			if(nicks[i].equals(nickname))
-				return false;
-		return true;
-	}
-	
-	private static void entrar(ChatClient cliente, ChatServer servidor) throws RemoteException, MalformedURLException, NotBoundException {
-		while(!(nickDisponivel(servidor)))
-			System.out.println("Nick não disponível. Tente outro.");
-		((ChatClientImpl) cliente).setNickname(nickname);
-		((ChatClientImpl) cliente).setServidor(servidor);
-		servidor.conectar(nickname, cliente);
-		cliente.evento("Você entrou na sala como " +nickname);
-		while(falar(servidor));
-	}
-	
-	private static boolean falar(ChatServer servidor) throws RemoteException {
-		String msg = teclado.nextLine();
-		if(msg.equals("Sair"))
-			return false;
-		servidor.falar(nickname, msg);
-		return true;
 	}
 }
